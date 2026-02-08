0b45686df15ea5734f41e1b8ec1e3c8c87a853ec
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-08 17:26:45 +0545

    Refactor code structure for improved readability and maintainability

---
+++ C language/Basics/basics.c
@@ -0,0 +1,23 @@
+//C Introduction
+//C is a general-purpose programming language created by Dennis Ritchie at Bell Labs in 1972.
+//It is a powerful and efficient language that is widely used for system programming, embedded systems, and application development.
+//C is a procedural programming language, which means that it focuses on writing procedures or routines to perform specific tasks.
+//C is a compiled language, which means that the source code is translated into machine code by a compiler before it can be executed.
+//C is a low-level language, which means that it provides direct access to memory and hardware, making it suitable for system programming and performance-critical applications.
+
+//What is C used for?
+/* C is often used to create programs that need to run fast and work closely with the computer.
+
+-Operating systems, such as parts of Windows, Linux, and macOS
+-Programs inside devices like cars, TVs, and home electronics
+-Software that needs to be fast, including databases and system tools
+-Game engines and programs that handle graphics
+-Core libraries that other programming languages rely on*/
+
+//C Syntax
+
+#include<stdio.h>
+int main(){
+    printf("Hello World!");
+    return 0;
+}
---
+++ C language/Interview programs/2.c
@@ -0,0 +1,10 @@
+// SWAP 2 NUMBERS USING THIRD VARIABLE
+#include<stdio.h>
+int main(){
+    int a=5, b= 10,temp;
+    temp = a;
+    a = b;
+    b= temp;
+    printf("a=%d b=%d",a,b);
+    return 0;
+}
