8def3748f3a714b30c35a9c46280124eca81f94b
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-09 20:41:38 +0545

    changes

--- C language/Interview programs/6.c
+++ C language/Interview programs/6.c
@@ -1,19 +1,15 @@
-//CALCULATOR USING SWITCH CASE
-#include<stdio.h>
-int main(){
-printf("Enter the first number: ");
-int a,b;
-scanf("%d",&a);
-printf("Enter the second number: ");
-scanf("%d",&b);
-printf("Enter the operator (+, -, *, /): ");
-char op = '+';
-scanf(" %c",&op);
-switch(op){
-    case '+':printf("The sum is %d",a+b);break;
-    case '-':printf("The difference is %d",a-b);break;
-    case '*':printf("The product is %d",a*b);break;
-    case '/':printf("The quotient is %d",a/b);break;
-    default:printf("Invalid operator");
-}
+ #include <stdio.h>
+
+int main() {
+    int a=10, b=5;
+    char op='+';
+
+    switch(op){
+        case '+': printf("%d", a+b); break;
+        case '-': printf("%d", a-b); break;
+        case '*': printf("%d", a*b); break;
+        case '/': printf("%d", a/b); break;
+        default: printf("Invalid");
+    }
+        return 0;
 }
