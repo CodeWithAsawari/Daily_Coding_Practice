a191d5c6aef1383a3655784171c630d4a47e0d24
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-10 21:02:38 +0545

    New changes added in file

--- C language/Basics/Quizz.c
+++ C language/Basics/Quizz.c
@@ -5,7 +5,8 @@ Expected Output:
 
 Enter a floating-point number (e.g., 3.14159): 75.35678
 The number rounded to two decimal places is: 75.36*/
-/*#include<stdio.h>
+
+#include<stdio.h>
 int main(){
     float pi_value;
     printf("Enter a floating-point number:");
@@ -14,7 +15,7 @@ int main(){
 
 
 return 0;
-}*/
+}
 
 /* 
 Exercise 2: Calculate Sum
@@ -26,6 +27,7 @@ Enter the first integer: 10
 Enter the second integer: 20
 The sum of 10 and 20 is: 30*/
 
+
 #include<stdio.h>
 int main() {
     int a,b,sum;
@@ -36,4 +38,84 @@ int main() {
     sum = a+b;
     printf("The sum of %d and %d is : %d",a,b,sum);
     return 0;
+} 
+
+ /*
+Exercise 3: Arithmetic Operations
+Practice Problem: Write a C program that calculates and prints the sum, difference, product, and quotient (division) of two integers. 
+ Given:
+
+int a, b;
+a = 10;
+b = 20;
+Expected Output:
+
+Sum: 20 + 10 = 30
+Difference: 20 - 10 = 10
+Product: 20 * 10 = 200
+Quotient: 20 / 10 = 2.00 */
+
+#include<stdio.h>
+int main(){
+    int a,b;
+    a =10;
+    b =20;
+    printf("Sum:%d + %d = %d\n",b,a,b+a);
+    printf("Difference:%d - %d = %d\n",b,a,b-a);
+    printf("Product:%d * %d = %d\n",b,a,b*a);
+      
+    if(b!=0){
+        printf("\n Quotient: %d/%d = %.2f\n",b,a,(float)a/b);
+    }
+    else{
+        printf("Division by zero is not allowed.\n");
+    }
 }
+
+/* Exercise 4: Circumference/Area of a Circle
+Practice Problem: Write a C program to calculate both the area and circumference of a circle, given its radius. Use a pre-defined constant for π with value 3.14159.
+
+Given:
+float radius;
+radius = 5;
+
+Expected Output:
+Area of the circle: 78.540
+Circumference of the circle: 31.416
+*/
+
+#include<stdio.h>
+int main(){
+    float Pi = 3.14159;
+    float radius;
+    scanf(%f",&radius);
+area = Pi*radius*radius;
+circumference = 2*Pi*radius;
+printf("Area of the circle: %.3f\n",area);
+printf("Circumference of the circle: %.3f\n",circumference);
+return 0;
+} 
+
+/*Exercise 5: Simple Interest
+Practice Problem: Calculate simple interest for a loan, given the Principal amount (P), Rate of interest (R), and Time in years (T) as input.
+
+Given:
+
+float principal, rate, time;
+principal = 1000;
+rate = 8;
+time = 3;
+Expected Output:
+
+Simple Interest is: 240.00*/
+
+#include<stdio.h>
+int main(){
+    float principal, rate, time, simple_interest;
+    principal = 1000;
+    rate = 8;
+    time = 3;
+    simple_interest = (principal * rate * time) / 100;
+    printf("Simple Interest is: %.2f\n", simple_interest);
+return 0;
+} 
