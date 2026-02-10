9cd82ca25f68418157c31a0d1201a80708d73b61
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-10 21:08:54 +0545

    new file added

---
+++ C language/Interview programs/8.c
@@ -0,0 +1,22 @@
+//Palindrome Number
+
+#include<stdio.h>
+int main(){
+    int num,original,rev=0,rem;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    original=num;
+
+    while(num!=0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+    }
+    if(original==rev){
+        printf("%d is a palindrome number.",original);
+    }
+    else{
+        printf("%d is not a palindrome number.",original);
+    }
+
+}
