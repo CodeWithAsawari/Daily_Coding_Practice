5ec536f4b33deb3ee63b39797c391137dc8d95a8
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-10 21:17:32 +0545

    new file added

---
+++ C language/Interview programs/10.c
@@ -0,0 +1,19 @@
+//Fibonacci Series
+#include<stdio.h>
+int main(){
+    int n,i,a=0,b=1,next;
+    printf("Enter the number of terms: ");
+    scanf("%d",&n);
+    printf("Fibonacci Series: ");
+    for(i=0;i<n;i++){
+        if(i<=1){
+            next=i;
+        }
+        else{
+            next=a+b;
+            a=b;
+            b=next;
+        }
+        printf("%d ",next);
+    }
+}
