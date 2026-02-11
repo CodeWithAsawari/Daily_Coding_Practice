943a73cae055e7bb178b45bab76a555dddb2d6ef
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-11 23:12:38 +0545

    new file added

---
+++ C language/DSA_programs.c
@@ -0,0 +1,17 @@
+// Linear Search
+ #include<stdio.h>
+int main(){
+int a[5]={10,20,30,40,50};
+int key=20,found=0;
+for(int i=0;i<5;i++){
+if (a[i]==key){
+printf("Element found at index %d",i+1);
+found=1;
+break;
+}}
+if(!found){
+printf("Element not found");
+}
+return 0;
+}
+ 
