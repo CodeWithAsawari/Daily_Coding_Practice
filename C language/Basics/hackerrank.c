f4c29a4fcc07db6ee47bff49fc82ca493e1d41f0
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-11 21:48:27 +0545

    changes added

--- C language/Basics/hackerrank.c
+++ C language/Basics/hackerrank.c
@@ -101,3 +101,28 @@ int main()
     return 0;
 }
 
+// CHALLENGE 4
+//Functions in C
+/*Objective
+
+In this challenge, you will learn simple usage of functions in C. Functions are a bunch of statements grouped together. A function is provided with zero or more arguments, and it executes the statements on it. Based on the return type, it either returns nothing (void) or something.
+Task
+
+Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them.*/
+#include <stdio.h>
+int max_of_four(int a, int b, int c, int d){
+    int max = a;
+    if(b > max) max = b;
+    if(c > max) max = c;
+    if(d > max) max = d;
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
