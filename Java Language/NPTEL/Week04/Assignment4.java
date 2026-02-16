83430b6a1615b87d5aa24913257237a1f2ae470c
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-16 22:48:30 +0545

    changes added

--- Java Language/NPTEL/Week04/Assignment4.java
+++ Java Language/NPTEL/Week04/Assignment4.java
@@ -1,5 +1,25 @@
- 
+ import java.util.Scanner;
 
 public class Assignment4 {
-    
+    static class Employee {
+
+        // Protected variable
+        protected int salary;
+    }
+
+    static class Manager extends Employee {
+        // Inherits salary
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int s = sc.nextInt();
+
+        Manager m = new Manager();
+        m.salary = s;
+System.out.println("Salary is: "+m.salary);
+        sc.close();
+    }
 }
