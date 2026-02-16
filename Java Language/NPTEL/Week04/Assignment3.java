7d9a3ff230a4440ad1b53f8d23e4c95ec2bbb472
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-16 22:47:15 +0545

    changes added

--- Java Language/NPTEL/Week04/Assignment3.java
+++ Java Language/NPTEL/Week04/Assignment3.java
@@ -1,4 +1,32 @@
- 
+ import java.util.Scanner;
 public class Assignment3 {
-    
+    static class Account {
+
+        // Private variable
+        private int balance;
+
+        // Method to set balance
+        public void setBalance(int b) {
+            balance = b;
+        }
+         
+public int getBalance() {
+  return balance;
 }
+}
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int b = sc.nextInt();
+
+        Account acc = new Account();
+        acc.setBalance(b);
+
+        System.out.println("Account Balance is: " + acc.getBalance());
+
+        sc.close();
+    }
+}
+
