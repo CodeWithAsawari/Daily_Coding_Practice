dc2259077fbd73099e670fcdaeafc491e605329f
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-16 22:50:13 +0545

    changes added

--- Java Language/NPTEL/Week04/Assignment5.java
+++ Java Language/NPTEL/Week04/Assignment5.java
@@ -1,4 +1,37 @@
- 
+ import java.util.Scanner;
 public class Assignment5 {
-    
+    static class Calculator {
+
+        // Public method
+        public int add(int a, int b) {
+            return a + b;
+        }
+         private int add(int a, int b, int c) {
+   return a + b + c; }  
+public int getThreeSum(int x, int y, int z) { 
+  return add(x, y, z); }
+
+public void printThreeSum(int x, int y, int z) {
+            int sum = add(x, y, z);
+            System.out.println("Sum of three numbers: " + sum);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+        Calculator calc = new Calculator();
+
+        System.out.println("Sum of two numbers: " + calc.add(a, b));
+        calc.printThreeSum(x, y, z);
+
+        sc.close();
+    }
 }
