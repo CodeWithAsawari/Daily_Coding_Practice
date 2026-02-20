3246feb53c1e644fde5f29c4f566fb31d33105ee
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-20 23:00:33 +0545

    Update Quizz.c

--- C language/Basics/Quizz.c
+++ C language/Basics/Quizz.c
@@ -1,11 +1,11 @@
-/* Exercise 1: Float Input/Output
+ Exercise 1: Float Input/Output
 Practice Problem: Write a C program that takes a floating-point number (a decimal number, like 3.14159) from the user and prints it, formatted to display with only two decimal places.
 
 Expected Output:
 
 Enter a floating-point number (e.g., 3.14159): 75.35678
 The number rounded to two decimal places is: 75.36*/
-/*
+
 #include<stdio.h>
 int main(){
     float pi_value;
