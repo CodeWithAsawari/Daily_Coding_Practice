5b32e3bfcd34dad94aa3ceb7ef014b338a6b2a3e
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-21 19:44:17 +0545

    changes added

--- Java Language/NPTEL/Week05/Assignment1.java
+++ Java Language/NPTEL/Week05/Assignment1.java
@@ -1,5 +1,17 @@
-package Daily_Coding_Practice.Java Language.NPTEL.Week05;
+ import java.util.Scanner;
 
-public 2 {
-    
+interface Number {
+    int findSqr(int i);  // Returns the square of n
+}
+ class Assignment1 implements Number {
+      public int findSqr(int i){
+    return i*i;
+  }
+     public static void main (String[] args){ 
+	      Assignment1 a = new Assignment1();   //Create an object of class Assignment1
+           // Read a number from the keyboard
+           Scanner sc = new Scanner(System.in);  
+           int i = sc.nextInt();
+           System.out.print(a.findSqr(i)); 
+    } 
 }
