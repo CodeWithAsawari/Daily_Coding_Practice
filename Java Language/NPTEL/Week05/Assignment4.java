2a9df66aad2d62a1499780ed5fdb85b58db12a0a
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-02-21 19:49:02 +0545

    changes added

--- Java Language/NPTEL/Week05/Assignment4.java
+++
@@ -1,5 +0,0 @@
-package Daily_Coding_Practice.Java Language.NPTEL.Week05;
-
-public class Assignment4 {
-    
-}
---
+++ Java Language/NPTEL/Week05/W05_P4.java
@@ -0,0 +1,27 @@
+ //Prefixed Fixed Code:
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class W05_P4 {
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in); 
+	int length = sc.nextInt(); 
+	// create an array to save user input 
+	int[] name = new int[length];
+     int sum=0;//save the total sum of the array.
+ 
+      try{
+       //#traverse the array to find the total sum of the array.
+       for(int i=0;i<length;i++){
+         name[i]=sc.nextInt();
+         sum+=name[i];
+     }
+       System.out.println(sum);
+     }
+     
+catch(InputMismatchException e) {
+        System.out.print("You entered bad data.");
+     }
+    
+}
+}
