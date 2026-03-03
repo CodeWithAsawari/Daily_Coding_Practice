8433afd81bf0372b10cfa5faf40671dcf5c24ba9
Asawari Mitragotri <asawarimitragotri@gmail.com>
2026-03-03 16:11:16 +0545

    changes added

--- Java Language/NPTEL/Week06/Assignment3.java
+++ Java Language/NPTEL/Week06/Assignment3.java
@@ -1,5 +1,21 @@
-
-
-public class Assignment3 {
-    
+ interface Task {
+    void run();
 }
+
+class BaseTask implements Task {
+    public void run() {}
+}
+class MyThread extends BaseTask {
+  
+  public void run() {
+    System.out.print("Java Threads");
+  }
+}
+public class Assignment3 {
+    public static void main(String[] args) {
+        MyThread t = new MyThread();
+        t.run();
+    }
+}
+
+ 
